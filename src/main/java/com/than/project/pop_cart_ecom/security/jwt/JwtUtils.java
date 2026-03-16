8e7f008b10266ab4a8e45046595f69f51335ceba
J.Garingan <jonathangaringan65@gmail.com>
2026-03-16 20:15:27 +0800

    Apply test spring security and jwt

---
+++ src/main/java/com/than/project/pop_cart_ecom/security/jwt/JwtUtils.java
@@ -0,0 +1,88 @@
+package com.than.project.pop_cart_ecom.security.jwt;
+
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+
+@Component
+public class JwtUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${spring.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+    @Value("${spring.app.jwtSecret}")
+    private String jwtSecret;
+
+    public String StringJwtFromHeader(HttpServletRequest request) {
+        String bearerToken =  request.getHeader("Authorization");
+        logger.debug("Authorization Header: {}", bearerToken);
+        if(bearerToken!=null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public String generateTokenFromUsername(UserDetails userDetails){
+        String username = userDetails.getUsername();
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date().getTime() + jwtExpirationMs)))
+                .signWith(key())
+                .compact();
+    }
+
+    public String getUsernameFromJwt(String token){
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody()
+                .getSubject();
+
+    }
+
+    public Key key(){
+        return Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(jwtSecret)
+        );
+    }
+
+    public boolean validateJwtToken(String authToken){
+        try{
+            System.out.println("Validate");
+
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJwt(authToken);
+            return true;
+        }catch(MalformedJwtException e){
+            logger.error("Invalid JWT Token: {}", e.getMessage());
+        }catch (ExpiredJwtException e){
+            logger.error("JWT token is expired: {}", e.getMessage());
+        }catch (UnsupportedJwtException e){
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        }catch (IllegalArgumentException e){
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+}
