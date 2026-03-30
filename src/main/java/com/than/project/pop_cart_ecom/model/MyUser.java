2ac466a183344c01aaf2a3912d87e789e5413fea
J.Garingan <jonathangaringan65@gmail.com>
2026-03-30 21:46:31 +0800

    Add Mapping relations to user and product

--- src/main/java/com/than/project/pop_cart_ecom/model/MyUser.java
+++ src/main/java/com/than/project/pop_cart_ecom/model/MyUser.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import lombok.ToString.Exclude;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -57,7 +58,7 @@ public class MyUser {
                 inverseJoinColumns = @JoinColumn(name="role_id"))
     private Set<Role> roles = new HashSet<>();
 
-    @ToString.Exclude
+    @Exclude
     @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE},
     orphanRemoval = true)
         private Set<Product> products;
@@ -71,7 +72,13 @@ public class MyUser {
     )
     private List<Address> addresses = new ArrayList<>();
 
-    public Set<Product> getProducts() {
+    @Exclude
+    @OneToOne(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    private Cart cart;
+    
+    @OneToMany(mappedBy = "user",
+    cascade = {CascadeType.PERSIST, CascadeType.MERGE}, orphanRemoval = true)
+    private Set<Product> products() {
         return products;
     }
 }
--- src/main/java/com/than/project/pop_cart_ecom/model/Product.java
+++ src/main/java/com/than/project/pop_cart_ecom/model/Product.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.List;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -33,4 +35,7 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "seller_id")
     private MyUser user;
+
+    @OneToMany(mappedBy = "product", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    private List<CartItem> products;
 }
