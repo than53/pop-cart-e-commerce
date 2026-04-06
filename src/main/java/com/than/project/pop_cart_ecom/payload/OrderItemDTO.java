51777ad431d8fba1c03308d1499233c725c01800
J.Garingan <jonathangaringan65@gmail.com>
2026-04-06 21:44:54 +0800

    Create repositories for order and payment and DTO

---
+++ src/main/java/com/than/project/pop_cart_ecom/payload/OrderItemDTO.java
@@ -0,0 +1,19 @@
+package com.than.project.pop_cart_ecom.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemDTO {
+
+    private Long orderItemId;
+    private ProductDTO product;
+    private Integer quantity;
+    private double discount;
+    private double orderedProductPrice;
+
+
+}
