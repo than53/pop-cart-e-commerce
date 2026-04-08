4b42532d51c00cf577adab357d91e317b478f53c
J.Garingan <jonathangaringan65@gmail.com>
2026-04-08 22:36:56 +0800

    Customize the info in my swagger

--- src/main/java/com/than/project/pop_cart_ecom/config/SwaggerConfig.java
+++ src/main/java/com/than/project/pop_cart_ecom/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.than.project.pop_cart_ecom.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +25,16 @@ public class SwaggerConfig {
                 .addList("Bearer Authentication");
 
         return  new OpenAPI()
+                .info(new Info()
+                        .title("Pop-cart-ecom API")
+                        .version("1.0")
+                        .description("This is a Personal Project for ecommerce")
+                        .license(new License().name("Apache 2.0").url("https://github.com/than53"))
+                        .contact(new Contact()
+                                .name("Jonathan Garingan")
+                                .email("kyang7051@gmail.com"))
+                )
+
                 .components(new Components()
                         .addSecuritySchemes("Bearer Authentication",bearerScheme))
                 .addSecurityItem(bearerRequirement);
